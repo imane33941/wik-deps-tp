ba96aed91d69684f1cff465d36555f981ce00238
bichon <imane.bichon@icloud.com>
2026-06-01 16:12:43 +0200

    first commit

---
+++ dist/server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+const PORT = process.env.PING_LISTEN_PORT || 3000;
+const server = http.createServer((req, res) => {
+    if (req.url === '/ping' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(req.headers));
+    }
+    else {
+        res.writeHead(404);
+        res.end('Not Found');
+    }
+});
+server.listen(PORT, () => {
+    console.log(`Serveur démarré sur ${PORT}`);
+});
