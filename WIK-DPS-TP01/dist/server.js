e5b7d789400a2be93663246fcc4b52001298ed1b
bichon <imane.bichon@icloud.com>
2026-06-02 11:20:30 +0200

    reorganize files for the first tp into a folder

--- WIK-DPS-TP01/dist/server.js
+++ WIK-DPS-TP01/dist/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
 const PORT = process.env.PING_LISTEN_PORT || 3000;
 const server = http.createServer((req, res) => {
+    console.log(`Requête reçue: ${req.url}`);
     if (req.url === '/ping' && req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(req.headers));
--- WIK-DPS-TP01/src/server.ts
+++ WIK-DPS-TP01/src/server.ts
@@ -2,7 +2,11 @@ import http from 'http';
 
 const PORT = process.env.PING_LISTEN_PORT || 3000;
 
+const app = http.createServer();
+
 const server = http.createServer((req, res) => {
+
+  console.log(`Requête reçue: ${req.url}`);
 
   if (req.url === '/ping' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
